cd3281f72dd89a9fac696b8b8042163658a16434
Bryce <bryceandy@rocketmail.com>
2018-09-16 00:46:28 +0300

    prices

--- resources/views/prices.blade.php
+++ resources/views/prices.blade.php
@@ -37,44 +37,44 @@
             <h2 class="text-left gree col-12">Massage Therapies</h2>
             <div class="col-12">
                 <ul class="priceLists">
-                    <li class="purp">Sports - 60min <span class="pin text-right">80,000</span></li>
-                    <li class="purp">Sports - 90min <span class="pin text-right">120,000</span></li>
-                    <li class="purp">Deep Tissue - 60min <span class="pin text-right">80,000</span></li>
-                    <li class="purp">Deep Tissue - 90min <span class="pin text-right">120,000</span></li>
-                    <li class="purp">Cupping Therapy - 60min <span class="pin text-right">80,000</span></li>
-                    <li class="purp">Hot Stone - 60min <span class="pin text-right">80,000</span></li>
-                    <li class="purp">Hot Stone - 90min <span class="pin text-right">120,000</span></li>
-                    <li class="purp">Body Slimming - 60min <span class="pin text-right">80,000</span></li>
-                    <li class="purp">Body Slimming - 90min <span class="pin text-right">120,000</span></li>
-                    <li class="purp">Prenatal - 60min <span class="pin text-right">80,000</span></li>
-                    <li class="purp">Aromatherapy - 60min <span class="pin text-right">80,000</span></li>
-                    <li class="purp">Aromatherapy - 90min <span class="pin text-right">120,000</span></li>
-                    <li class="purp">Swedish - 60min <span class="pin text-right">80,000</span></li>
-                    <li class="purp">Swedish - 90min <span class="pin text-right">120,000</span></li>
-                    <li class="purp">Thai - 60min <span class="pin text-right">80,000</span></li>
-                    <li class="purp">Thai - 90min <span class="pin text-right">120,000</span></li>
-                    <li class="purp">Head - 30min <span class="pin text-right">40,000</span></li>
-                    <li class="purp">Reflexology - 30min <span class="pin text-right">40,000</span></li>
-                    <li class="purp">Body Scrub - 60min <span class="pin text-right">80,000</span></li>
-                    <li class="purp">Sauna - 30min <span class="pin text-right">40,000</span></li>
+                    <li class="purp">Sports - 60min <span class="pin text-right">60,000</span></li>
+                    <li class="purp">Sports - 90min <span class="pin text-right">100,000</span></li>
+                    <li class="purp">Deep Tissue - 60min <span class="pin text-right">60,000</span></li>
+                    <li class="purp">Deep Tissue - 90min <span class="pin text-right">100,000</span></li>
+                    <li class="purp">Cupping Therapy - 60min <span class="pin text-right">60,000</span></li>
+                    <li class="purp">Hot Stone - 60min <span class="pin text-right">60,000</span></li>
+                    <li class="purp">Hot Stone - 90min <span class="pin text-right">100,000</span></li>
+                    <li class="purp">Body Slimming - 60min <span class="pin text-right">60,000</span></li>
+                    <li class="purp">Body Slimming - 90min <span class="pin text-right">100,000</span></li>
+                    <li class="purp">Prenatal - 60min <span class="pin text-right">60,000</span></li>
+                    <li class="purp">Aromatherapy - 60min <span class="pin text-right">60,000</span></li>
+                    <li class="purp">Aromatherapy - 90min <span class="pin text-right">100,000</span></li>
+                    <li class="purp">Swedish - 60min <span class="pin text-right">60,000</span></li>
+                    <li class="purp">Swedish - 90min <span class="pin text-right">100,000</span></li>
+                    <li class="purp">Thai - 60min <span class="pin text-right">60,000</span></li>
+                    <li class="purp">Thai - 90min <span class="pin text-right">100,000</span></li>
+                    <li class="purp">Head - 30min <span class="pin text-right">30,000</span></li>
+                    <li class="purp">Reflexology - 30min <span class="pin text-right">30,000</span></li>
+                    <li class="purp">Body Scrub - 60min <span class="pin text-right">60,000</span></li>
+                    <li class="purp">Sauna - 30min <span class="pin text-right">30,000</span></li>
                 </ul>
             </div><br /><br />
 
             <h2 class="text-left gree col-12">Waxing Treatments</h2>
             <div class="col-12">
                 <ul class="priceLists">
-                    <li class="purp">Brazilian Bikini <span class="pin text-right">25,000</span></li>
+                    <li class="purp">Brazilian Bikini <span class="pin text-right">20,000</span></li>
                     <li class="purp">Under Arm <span class="pin text-right">10,000</span></li>
-                    <li class="purp">Full Leg <span class="pin text-right">60,000</span></li>
-                    <li class="purp">Half Leg <span class="pin text-right">30,000</span></li>
+                    <li class="purp">Full Leg <span class="pin text-right">30,000</span></li>
+                    <li class="purp">Half Leg <span class="pin text-right">15,000</span></li>
                     <li class="purp">Lip Waxing <span class="pin text-right">10,000</span></li>
-                    <li class="purp">Cheek Waxing <span class="pin text-right">20,000</span></li>
+                    <li class="purp">Cheek Waxing <span class="pin text-right">15,000</span></li>
                     <li class="purp">Chin Waxing <span class="pin text-right">10,000</span></li>
-                    <li class="purp">Full Arm <span class="pin text-right">40,000</span></li>
-                    <li class="purp">Half Arm <span class="pin text-right">20,000</span></li>
-                    <li class="purp">Chest & Stomach <span class="pin text-right">50,000</span></li>
-                    <li class="purp">Full Back <span class="pin text-right">50,000</span></li>
-                    <li class="purp">Full Body <span class="pin text-right">300,000</span></li>
+                    <li class="purp">Full Arm <span class="pin text-right">30,000</span></li>
+                    <li class="purp">Half Arm <span class="pin text-right">15,000</span></li>
+                    <li class="purp">Chest & Stomach <span class="pin text-right">30,000</span></li>
+                    <li class="purp">Full Back <span class="pin text-right">30,000</span></li>
+                    <li class="purp">Full Body <span class="pin text-right">200,000</span></li>
                 </ul>
             </div><br /><br />
 
