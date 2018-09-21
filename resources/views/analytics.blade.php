3082167ae8126f2b18ed7d13bfd1f7e61b9872a3
Bryce <bryceandy@rocketmail.com>
2018-09-22 00:52:57 +0300

    analytics

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -8,16 +8,16 @@
 
 <h1 class="text-center">Visitors and Page Views</h1>
 
-    <p>
-        @foreach($analyticsData as $data)
 
-            <ul class="list-group">
-                <li class="list-group-item">{{$data['date']}}</li>
-                <li class="list-group-item">{{$data['visitors']}}</li>
-                <li class="list-group-item">{{$data['pageViews']}}</li>
-            </ul>
-            <hr>
-        @endforeach
-    </p>
+    @foreach($analyticsData as $data)
+
+        <ul class="list-group">
+            <li class="list-group-item">{{$data['date']}}</li>
+            <li class="list-group-item">{{$data['visitors']}}</li>
+            <li class="list-group-item">{{$data['pageViews']}}</li>
+        </ul>
+        <hr>
+    @endforeach
+
 
 @endsection
