5899eece03b5a788cc6c82295f62357ef60b2224
Bryce <bryceandy@rocketmail.com>
2018-09-22 03:20:44 +0300

    analytics

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -11,7 +11,7 @@
 
     @foreach($analyticsData as $data)
 
-        <ul class="list-group w-75" style="margin-left: 5%;">
+        <ul class="list-group w-75" style="margin-left: 10vw;">
             <li class="list-group-item" style="color: #D50B53;">Day - {{$data['date']}}</li>
             <li class="list-group-item" style="color: #A882C1;">Visitors - {{$data['visitors']}}</li>
             <li class="list-group-item" style="color: #B9C406;">Page Views - {{$data['pageViews']}}</li>
