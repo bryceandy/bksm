33741b145446a4c91d80cddaa2b0f9a4ceb60618
Bryce <bryceandy@rocketmail.com>
2018-09-22 03:13:14 +0300

    analytics

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -11,12 +11,12 @@
 
     @foreach($analyticsData as $data)
 
-        <ul class="list-group">
-            <li class="list-group-item" style="color: #D50B53;">{{$data['date']}}</li>
-            <li class="list-group-item" style="color: #A882C1;">{{$data['visitors']}}</li>
-            <li class="list-group-item" style="color: #B9C406;">{{$data['pageViews']}}</li>
+        <ul class="list-group w-50">
+            <li class="list-group-item" style="color: #D50B53;">Day - {{$data['date']}}</li>
+            <li class="list-group-item" style="color: #A882C1;">Visitors - {{$data['visitors']}}</li>
+            <li class="list-group-item" style="color: #B9C406;">Page Views - {{$data['pageViews']}}</li>
+            <li class="list-group-item">Page Title - {{$data['pageTitle']}}</li>
         </ul>
-        <hr>
     @endforeach
 
 @endsection
