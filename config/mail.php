dabacd310b939d16ad2cef0b0e98fe52b203c528
Bryce <bryceandy@rocketmail.com>
2018-12-04 21:25:41 +0300

    mail

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'info@bksportsmassage.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'brendakinariki@hotmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
